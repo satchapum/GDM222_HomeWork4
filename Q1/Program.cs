2d4d787740a2d4c1c1059008fd2be37c10f6215a
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2023-04-24 15:58:27 +0700

    finish

---
+++ Q1/Program.cs
@@ -0,0 +1,62 @@
+﻿class Program 
+{
+    public static void Main(string[] Args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        List<List<int>> node = new List<List<int>>();  
+        for(int i = 0; i < number; i++)
+        {
+            List<int> edgeTemp = new List<int>();
+            node.Add(edgeTemp);
+        }
+        while(true)
+        {
+            int inputnode = int.Parse(Console.ReadLine());
+            
+            if(inputnode < 0 || inputnode >= number)
+            {
+                break;
+            }
+            else
+            {
+                int inputedge = int.Parse(Console.ReadLine());
+                node[inputnode].Add(inputedge);
+            }
+        }
+        int start = int.Parse(Console.ReadLine());
+        int target = int.Parse(Console.ReadLine());
+        bool[] visited = new bool[number+1]; 
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(start);
+
+        while(queue.Count > 0)
+        {
+            int next = queue.Dequeue();
+            if(visited[next])
+            {
+                continue;
+            }
+            else
+            {
+                visited[next] = true;
+            }
+
+            if(next == target)
+            {
+                break;
+            }
+            foreach(int i in node[next])
+            {
+                queue.Enqueue(i);
+            }
+        }
+        if(visited[target])
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program 
+{
+    public static void Main(string[] Args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        List<List<int>> node = new List<List<int>>();  
+        for(int i = 0; i < number; i++)
+        {
+            List<int> edgeTemp = new List<int>();
+            node.Add(edgeTemp);
+        }
+        while(true)
+        {
+            int inputnode = int.Parse(Console.ReadLine());
+            
+            if(inputnode < 0 || inputnode >= number)
+            {
+                break;
+            }
+            else
+            {
+                int inputedge = int.Parse(Console.ReadLine());
+                node[inputnode].Add(inputedge);
+                node[inputedge].Add(inputnode);
+            }
+        }
+
+        int[] color = new int[number+1]; 
+
+        for(int i = 0; i < number; i++)
+        {
+            foreach(int x in node[i])
+            {
+                if(color[i] == color[x])
+                {
+                    color[x]++;
+                }
+            }
+        }
+        int temp = 0;
+        foreach(int x in color)
+        {
+            if(x > temp)
+            {
+                temp = x;
+            }
+        }
+        Console.WriteLine(temp + 1);
+    }
+}
